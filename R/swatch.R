8b22115e557ac120684c61d8cfdcce4090fc000c
cttobin <cttobin@hotmail.com>
2013-11-16 13:20:23 +0000

    Updated swatch() documentation to match changed function arguments.

--- R/swatch.R
+++ R/swatch.R
@@ -1,32 +1,21 @@
-#' Retrieve the swatch colours from a ggthemr object
-#' 
-#' The swatch is the main set of colours used for plotting data in the a ggplot2 plot. This definition excludes the colours used for the plot background, title/axis text, gridlines etc. 
-#' @param themr ggthemr object.
-#' @param preserve_object FALSE (default) will return a character vector of the colours in the swatch. TRUE will return a ggthemr swatch object.
+#' @title Retrieve the swatch colours from a ggthemr object
+#' @description The swatch is the main set of colours used for plotting data in a ggplot2 plot. This definition excludes the colours used for the plot background, title/axis text, gridlines etc. 
+#' @param x ggthemr or ggthemr palette object.
 #' @examples
-#' themr <- ggthemr('pale')
+#' themr <- ggthemr('sea')
 #' swatch(themr)
 #' @rdname swatch
 #' @export swatch
+swatch <- function(x) UseMethod('swatch', x)
 
-swatch <- function(x) 
-  UseMethod('swatch', x)
 
-#' @return \code{NULL}
-#'
 #' @rdname swatch
 #' @method swatch ggthemr
 #' @S3method swatch ggthemr
-swatch.ggthemr <- function(x) {
-  return (x$palette$object$swatch)
-}
+swatch.ggthemr <- function(x) x$palette$object$swatch
 
 
-#' @return \code{NULL}
-#'
 #' @rdname swatch
 #' @method swatch ggthemr.palette
 #' @S3method swatch ggthemr.palette
-swatch.ggthemr.palette <- function(x) {
-  return (x$swatch)
-}
+swatch.ggthemr.palette <- function(x) x$swatch
