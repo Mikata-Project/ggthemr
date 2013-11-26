d81d6ac045a88a85a79ccee556eab52c05164d1e
cttobin <cttobin@hotmail.com>
2013-11-26 23:14:54 +0000

    Better previews in preview_theme().

--- R/preview_theme.R
+++ R/preview_theme.R
@@ -5,12 +5,12 @@ preview_theme  <- function(themr = NULL, save_to = NULL) {
   
   driver_x <- scale_x_discrete(breaks = seq(1969, 1984, 3), label = function(x) paste0("'", substr(x, 3, 4)))  
   display_theme <- list(theme(legend.position = 'none'), scale_y_continuous(label = function(x) format(x, big.mark = ",", scientific = FALSE)))
-  
+
   display <- list(
     ggplot(diamonds, aes(price, fill = factor(cut))) + geom_histogram(binwidth = 850) + display_theme + xlab('Price (USD)') + ylab('Count') + scale_x_continuous(label = function(x) paste0(x / 1000, 'k')),
-    ggplot(as.data.frame(table(diamonds$clarity, dnn = 'Clarity'), responseName = 'Count'), aes(Clarity, Count)) + geom_bar(stat = 'identity') + display_theme + theme(axis.text.x = element_text(angle = 0, hjust = 1)),
+    ggplot(mtcars, aes(wt, mpg)) + geom_abline(intercept = 37, slope = -5, linetype = 'dotted') + geom_point(aes(shape = factor(cyl))) + labs(shape = 'Cylinders', x = 'Weight', y = 'MPG'),
     ggplot(drivers, aes(Year, Deaths)) + geom_boxplot(size = 0.25) + ylab('Monthly Deaths')+ display_theme + driver_x,
-    ggplot(aggregate(Deaths ~ Year, drivers, sum), aes(Year, Deaths, group = 1)) + geom_smooth(method = 'loess') + geom_line() + geom_point()  + display_theme + driver_x
+    ggplot(mtcars, aes(mpg, fill = factor(cyl), colour = factor(cyl))) + geom_density(alpha = 0.75) + labs(fill = 'Cylinders', colour = 'Cylinders', x = 'MPG', y = 'Density') 
   )
   
   if (!is.null(themr)) {
