0547d784f3c266599572387e81ed41d1a1e7e74f
cttobin <cttobin@hotmail.com>
2013-12-01 18:12:13 +0000

    Fixed incorrect usage of '$' with a nice '[['.

--- R/theme_scales.R
+++ R/theme_scales.R
@@ -1,5 +1,5 @@
 # Needs some serious tidying.
-theme_scales <- function(palette) {
+theme_scales <- function(palette, line_weight) {
   
   colours <- swatch(palette)[-1]
   
@@ -8,24 +8,24 @@ theme_scales <- function(palette) {
   
   # Geoms that only require a default colour.
   updateGeoms(c('abline', 'jitter', 'point', 'density', 'errorbar', 'errorbarh', 'freqpoly', 'hline', 'line', 'text'), list(colour = colours[1]))
-  
+    
   # Geoms that only require a default fill.
   updateGeoms(c('area', 'bar', 'histogram', 'dotplot'), list(fill = colours[1]))
   
   # Special geoms.
-  update_geom_defaults('boxplot', list(colour = palette$swatch[1], fill = colours[1], outlier.colour = palette$swatch[1]))
+  update_geom_defaults('boxplot', list(colour = palette$swatch[1], fill = colours[1], outlier.colour = palette$swatch[1], size = line_weight))
   update_geom_defaults('smooth', list(colour = colours[2], fill = colours[2]))
   update_geom_defaults('dotplot', list(colour = colours[1], fill = colours[1]))
   
   # Discrete scales.
-  assign('scale_colour_discrete', function(...) discrete_scale('colour', 'ggthemr', discrete_colours(colours), ...), envir=ggthemr_env)
-  assign('scale_fill_discrete', function(...) discrete_scale('fill', 'ggthemr', discrete_colours(colours), ...), envir=ggthemr_env)
+  assign('scale_colour_discrete', function(...) discrete_scale('colour', 'ggthemr', discrete_colours(colours), ...), envir=.GlobalEnv)
+  assign('scale_fill_discrete', function(...) discrete_scale('fill', 'ggthemr', discrete_colours(colours), ...), envir=.GlobalEnv)
   
   # Continuous scales.
-  assign('scale_colour_continuous', function(...) continuous_scale('colour', 'ggthemr', scales::seq_gradient_pal(palette$gradient[['low']], palette$gradient[['high']]), ...), envir=ggthemr_env)
-  assign('scale_fill_continuous', function(...) continuous_scale('fill', 'ggthemr', scales::seq_gradient_pal(palette$gradient[['low']], palette$gradient[['high']]), ...), envir=ggthemr_env)
+  assign('scale_colour_continuous', function(...) continuous_scale('colour', 'ggthemr', scales::seq_gradient_pal(palette$gradient[['low']], palette$gradient[['high']]), ...), envir=.GlobalEnv)
+  assign('scale_fill_continuous', function(...) continuous_scale('fill', 'ggthemr', scales::seq_gradient_pal(palette$gradient[['low']], palette$gradient[['high']]), ...), envir=.GlobalEnv)
   
   # Gradient scales.
-  assign('scale_colour_gradient', function(...) continuous_scale('colour', 'ggthemr', scales::seq_gradient_pal(palette$gradient$low, palette$gradient$high), ...), envir=ggthemr_env)
-  assign('scale_fill_gradient', function(...) continuous_scale('fill', 'ggthemr', scales::seq_gradient_pal(palette$gradient$low, palette$gradient$high), ...), envir=ggthemr_env)
+  assign('scale_colour_gradient', function(...) continuous_scale('colour', 'ggthemr', scales::seq_gradient_pal(palette$gradient[['low']], palette$gradient[['high']]), ...), envir=.GlobalEnv)
+  assign('scale_fill_gradient', function(...) continuous_scale('fill', 'ggthemr', scales::seq_gradient_pal(palette$gradient[['low']], palette$gradient[['high']]), ...), envir=.GlobalEnv)
 }
