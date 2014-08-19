1a9453a45287339187ecc6a368168a79d23de96c
cttobin <cttobin@tcd.ie>
2014-08-20 00:08:24 +0100

    Made preview_layout() generic function.

--- R/preview_layout.R
+++ R/preview_layout.R
@@ -1,4 +1,7 @@
-preview_layout <- function() {
+preview_layout <- function (x = NULL)
+  UseMethod('preview_layout', x)
+
+preview_layout.default <- function (x = NULL) {
   blank_data <- data.frame(
     x = c('One', 'Two', 'Three'),
     y = seq(1000, 3000, 1000),
@@ -6,4 +9,17 @@ preview_layout <- function() {
   ggplot(blank_data, aes(x, y)) + geom_blank() +
     scale_y_continuous(label = function(x) format(x, big.mark = ",", scientific = FALSE, trim = TRUE)) +
     labs(x = 'X Title', y = 'Y Title', title = 'Title')
+}
+
+preview_layout.character <- function (x) {
+  old <- theme_get()
+  ggthemr(layout = x)
+  print(preview_layout())
+  theme_set(old)
+}
+
+preview_layout.factor <- preview_layout.character
+
+preview_layout.ggthemrLayout <- function (x) {
+  
 }
