66bbc8292a2160bdf8e65907f4231025e875a2a7
cttobin <cttobin@tcd.ie>
2014-08-19 23:55:42 +0100

    define_palette() updated.

--- R/define_palette.R
+++ R/define_palette.R
@@ -7,40 +7,64 @@
 #' @param text_outer text colour when type is 'outer' (see plot_theme).
 #' @param line axes line colour
 #' @param gridline major and minor gridline colours.
-#' @param plot_colours vector of colours used for plotting data i.e. the main colours used in the plot.
+#' @param swatch vector of colours used for plotting data i.e. the main colours used in the plot.
 #' @param gradient vector of length two specifiying the lower and upper colours used for gradients.
 #' @author Ciaran Tobin
 
-define_palette <- function(background, text_inner, text_outer, line, gridline, plot_colours, gradient) {
+define_palette <- function(background, text, line, gridline, swatch, gradient) {
   
+  n_text <- length(text)
+  if (n_text == 1L) {
+    warning('You should supply a vector of length for the text colour. The first element being the colour used for plots of type "inner". The second when for plots of type "outer".', call. = FALSE)
+    text_inner <- text
+    text_outer <- text
+  } else if (n_text > 2L) {
+    warning('You have supplied two many colours for text colour. Only the first two will be used.', call. = FALSE)
+  } else {
+    text_inner <- text[1L]
+    text_outer <- text[2L]
+  }
+  
+  n_line <- length(line)
+  if (n_line == 1L) {
+    warning('You should supply a vector of length for the axis line colour. The first element being the colour used for plots of type "inner". The second when for plots of type "outer".', call. = FALSE)
+    line_inner <- line
+    line_outer <- line
+  } else if (n_line > 2L) {
+    warning('You have supplied two many colours for the axes line colour. Only the first two will be used.', call. = FALSE)
+  } else {
+    line_inner <- line[1L]
+    line_outer <- line[2L]
+  }
+    
   # Must have two gradient colours.
   n_gradient <- length(gradient)
   if (n_gradient != 2)
     stop("You must provide two gradient colours (low and high respectively).")
   
-  n_colours <- length(plot_colours)
+  n_colours <- length(swatch)
   if (n_colours < 6)
     warning(paste0('It is recommended that you supply at least 6 colours for the main palette (only ', 
-                   n_colours, ' given).'))
+                   n_colours, ' given).'), call. = FALSE)
   
   # Make sure there aren't silly colour combinations.
   if (gradient[1] == gradient[2]) 
-    stop('The gradient colours are the same.')
+    stop('The gradient colours are the same.', call. = FALSE)
     
-  if (any(duplicated(sapply(plot_colours, unname_colour)))) warning('Duplicate plot colours.')
-  clash_warning_any('Some plot colours are the same as the background.', plot_colours, background)
+  if (any(duplicated(sapply(swatch, unname_colour)))) warning('Duplicate plot colours.')
+  clash_warning_any('Some plot colours are the same as the background.', swatch, background)
   clash_warning_any('Some gradient colours are the same as the background.', gradient, background)  
   clash_error("The outer text colour is the same as the background colour. Text will not be visible.", background, text_outer)
   clash_error("The inner text colour is white. Text will not be visible.", text_inner, '#ffffff')
-  clash_warning("The line colour is the same as the background colour. Axis lines will not be visible. If this is desired it is more appropriate to define a new layout where axis lines are not present.", line, background)
+  clash_warning("The line colour is the same as the background colour. Axis lines will not be visible. If this is desired it is more appropriate to use new layout where axes lines are not present.", line, background)
   clash_warning("The gridline colour is the same as the background colour. Gridlines will not be visible. If this is desired it is more appropriate to define a new layout where gridlines are not present.", gridline, background)
     
   palette <- list(
     background = background, 
-    text = list(inner = text_inner, outer = text_outer), 
-    line = line,
+    text = c(inner = text_inner, outer = text_outer), 
+    line = c(inner = line_inner, outer = line_outer),
     gridline = gridline,
-    colours = unname(plot_colours),
+    swatch = unname(swatch),
     gradient = unname(gradient)
   )
   
