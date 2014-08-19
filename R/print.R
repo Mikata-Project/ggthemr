9ad3cca023c576189af7c0716b729eb606bb4d5b
cttobin <cttobin@tcd.ie>
2014-08-19 23:31:48 +0100

    Removed now unwanted print method.

--- R/print.R
+++
@@ -1,9 +0,0 @@
-#' Display ggthemr object properties
-#' @param x ggthemr object.
-#' @method print ggthemr
-print.ggthemr <- function(x) {
-  cat('Palette:   ', x$palette$name, '\n',
-      'Layout:    ', x$layout$name, '\n',
-      'Text size: ', x$text_size, '\n',
-      'Spacing:   ', x$spacing, sep='')
-}
