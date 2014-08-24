819f7cb0b1605df8861c235ff72a0452cf82706e
cttobin <cttobin@hotmail.com>
2014-08-24 15:07:18 +0100

    Working define_palette() method.

--- R/define_palette.R
+++ R/define_palette.R
@@ -1,17 +1,22 @@
-#' Create ggthemr palette.
+#' @title Create ggthemr palette
+#' @description Create a custom palette that can be used by ggthemr. Checks will be carried out to make sure colours are not duplicated and are appropriate (e.g. the text colour should not match the background colour of text will not be visible). 
+#' @param swatch Vector of colours used for plotting data i.e. the main colours used in the plot.
+#' @param gradient Vector of length two specifiying the lower and upper colours used for gradients.
+#' @param background Background colour.
+#' @param text Text colour. This should be either a single colour or a vector of two colours (see details). 
+#' @param line Axes line colour. This should be either a single colour or a vector of two colours (see details).
+#' @param gridline Major and minor gridline colours.
+#' @details The text and axes line colours can both be provided as single colours or a vector of two colours. If only one colour is provided then the text and line colours will be identical regardless of the theme type (inner or outer, see (\code{\link{ggthemr}}) for more details). Where your palette doesn't really need different text/line colours for the theme type than it sufficient to supply only one colour but a warning will be issued.
 #' 
-#' Create a custom palette that can be used by ggthemr.
-#' 
-#' @param background background colour.
-#' @param text_inner text colour when type is 'inner' (see plot_theme).
-#' @param text_outer text colour when type is 'outer' (see plot_theme).
-#' @param line axes line colour
-#' @param gridline major and minor gridline colours.
-#' @param swatch vector of colours used for plotting data i.e. the main colours used in the plot.
-#' @param gradient vector of length two specifiying the lower and upper colours used for gradients.
+#' Vectors supplied for the text and line colours can have elements named "inner" and "outer". In the absense of these names, the first will be assumed to be the "inner" colour, the second will be the "outer" colour.
 #' @author Ciaran Tobin
-
-define_palette <- function(background, text, line, gridline, swatch, gradient) {
+#' @export
+define_palette <- function(swatch, gradient,
+  background = '#ffffff', 
+  text = c('#444444', '#444444'), 
+  line = c('#6e6e6e', '#6e6e6e'), 
+  gridline = '#c3c3c3'
+) {
   
   n_text <- length(text)
   if (n_text == 1L) {
@@ -36,27 +41,17 @@ define_palette <- function(background, text, line, gridline, swatch, gradient) {
     line_inner <- line[1L]
     line_outer <- line[2L]
   }
-    
-  # Must have two gradient colours.
-  n_gradient <- length(gradient)
-  if (n_gradient != 2)
-    stop("You must provide two gradient colours (low and high respectively).")
-  
-  n_colours <- length(swatch)
-  if (n_colours < 6)
-    warning(paste0('It is recommended that you supply at least 6 colours for the main palette (only ', 
-                   n_colours, ' given).'), call. = FALSE)
-  
-  # Make sure there aren't silly colour combinations.
-  if (gradient[1] == gradient[2]) 
-    stop('The gradient colours are the same.', call. = FALSE)
-    
+      
   if (any(duplicated(sapply(swatch, unname_colour)))) warning('Duplicate plot colours.')
   clash_warning_any('Some plot colours are the same as the background.', swatch, background)
   clash_warning_any('Some gradient colours are the same as the background.', gradient, background)  
+  
+  clash_error("The inner text colour is the same as the background colour. Text will not be visible.", background, text_inner)
   clash_error("The outer text colour is the same as the background colour. Text will not be visible.", background, text_outer)
-  clash_error("The inner text colour is white. Text will not be visible.", text_inner, '#ffffff')
-  clash_warning("The line colour is the same as the background colour. Axis lines will not be visible. If this is desired it is more appropriate to use new layout where axes lines are not present.", line, background)
+  
+  clash_warning("The inner line colour is the same as the background colour. Axes lines will not be visible. If this is desired it is more appropriate to a layout where axes lines are not present.", line_inner, background)
+  clash_warning("The outer line colour is the same as the background colour. Axes lines will not be visible. If this is desired it is more appropriate to a layout where axes lines are not present.", line_outer, background)
+
   clash_warning("The gridline colour is the same as the background colour. Gridlines will not be visible. If this is desired it is more appropriate to define a new layout where gridlines are not present.", gridline, background)
     
   palette <- list(
@@ -64,12 +59,12 @@ define_palette <- function(background, text, line, gridline, swatch, gradient) {
     text = c(inner = text_inner, outer = text_outer), 
     line = c(inner = line_inner, outer = line_outer),
     gridline = gridline,
-    swatch = unname(swatch),
-    gradient = unname(gradient)
+    swatch = define_swatch(swatch),
+    gradient = define_gradient(gradient)
   )
   
   if (!all(sapply(unlist(palette), is.character)))
-    stop('You must only supply colours as characters.')
+    stop('You must only supply colours as characters.', call. = FALSE)
   
   class(palette) <- 'ggthemr_palette'
   
@@ -97,3 +92,23 @@ clash_warning <- function(warning_message, ...)
 
 clash_error <- function(stop_message, ...)
   if (colour_clash(...)) stop(stop_message, call. = FALSE)
+
+define_gradient <- function (x) {
+  
+  if (length(x) < 2L)
+    stop("You must provide two gradient colours (low and high respectively).", call. = FALSE)
+  
+  if (length(x) > 2L) 
+    warning('You have supplied too many colours for the gradient. Taking only the first two or those named "low" and "high".', call. = FALSE)
+  
+  if (x[[1L]] == x[[2L]])
+    stop("The gradient colours are the same.", call. = FALSE)
+  
+  nm <- names(x)
+  if (length(nm) == 2L & all(c('low', 'high') %in% nm)) {
+    return (c(low = x[['low']], high = x[['high']]))
+  } else {
+    return (c(low = x[[1L]], high = x[[2L]]))
+  }
+  
+}
--- R/define_swatch.R
+++ R/define_swatch.R
@@ -1,10 +1,13 @@
 define_swatch <- function(x) {
   n <- length(x)
   if (n < 1) 
-    stop('No colours provided.')
+    stop('No colours provided for swatch.', call. = FALSE)
+  if (n < 2) 
+    stop('You must have at least two colours in the swatch (seven recommended).', call. = FALSE)
   validate_colours(x)
   if (n < 6) 
-    warning('It is recommended that you provide at least six colours for the swatch.', call. = FALSE)
-  if (any(duplicated(sapply(x, unname_colour)))) warning('Duplicate colours.', call. = FALSE)
+    warning('It is recommended that you provide at least seven colours for the swatch.', call. = FALSE)
+  if (any(duplicated(sapply(x, unname_colour)))) 
+    warning('Duplicate colours in swatch.', call. = FALSE)
   structure(x, class = 'ggthemr_swatch')
 }
