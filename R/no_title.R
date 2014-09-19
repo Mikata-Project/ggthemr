ff9fee21c99c85d854f6710be2ebbed09a0354f7
Ciarán Tobin <ctobin@deloitte.ie>
2014-09-19 14:41:52 +0100

    Renamed rotate labels functions, removed some unused parameters and updated documentation.

--- R/no_title.R
+++ R/no_title.R
@@ -1,6 +1,5 @@
 #' @title Remove ggplot2 title
 #' @export
 #' @author Ciaran Tobin
-#' @seealso 
 no_title <- function () 
   theme(plot.title = element_text(hjust = 0))
--- R/rotate_labels.R
+++ R/rotate_labels.R
@@ -1,16 +1,25 @@
+#' @title Rotate ggplot2 axes labels
+#' @description Rotate both axes labels by a given angle.
+#' @param angle The angle of rotation.
+#' @author Ciaran Tobin
+#' @export
+rotate_axes_text <- function (angle) 
+  theme(axis.text = element_text(angle = angle))
+
+
 #' @title Rotate ggplot2 x-axis labels
 #' @description Rotate the x-axis labels/text by a given angle.
 #' @param angle The angle of rotation.
-#' @param align
 #' @author Ciaran Tobin
 #' @export
-rotate_x_labels <- function (angle, align = TRUE) 
+rotate_x_text <- function (angle) 
   theme(axis.text.x = element_text(angle = angle))
+
 
 #' @title Rotate ggplot2 x-axis labels
 #' @description Rotate the x-axis labels/text by a given angle.
 #' @inheritParams rotate_x_labels
 #' @author Ciaran Tobin
 #' @export
-rotate_y_labels <- function (angle, align = TRUE) 
+rotate_y_text <- function (angle) 
   theme(axis.text.y = element_text(angle = angle))
