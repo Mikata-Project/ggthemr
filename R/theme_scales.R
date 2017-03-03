9fc92b25f92c92eb262e9b0e7aed6c7f1e8e97fe
Sainath Adapa <adapasainath@gmail.com>
2017-03-03 14:37:41 +0530

    separating the creation of geoms/scales from the setting part

--- R/theme_scales.R
+++ R/theme_scales.R
@@ -1,25 +1,9 @@
 #' @importFrom scales seq_gradient_pal
-theme_scales <- function(palette, line_weight, pos, envir) {
+theme_scales <- function(palette) {
   
   colours <- palette$swatch[-1]
   
-  updateGeoms <- function(geoms, parameters) 
-    for (geom in geoms) update_geom_defaults(geom, parameters)
-  
-  # Geoms that only require a default colour.
-  updateGeoms(c('abline', 'point', 'density', 'errorbar', 'errorbarh', 'hline', 'line'), list(colour = colours[1]))
-  
-  update_geom_defaults('text', list(colour = palette$swatch[1]))
-    
-  # Geoms that only require a default fill.
-  updateGeoms(c('area', 'bar', 'dotplot'), list(fill = colours[1]))
-  
-  # Special geoms.
-  update_geom_defaults('boxplot', list(colour = palette$swatch[1], fill = colours[1], size = line_weight))
-  update_geom_defaults('smooth',  list(colour = colours[2],        fill = colours[2]))
-  update_geom_defaults('dotplot', list(colour = colours[1],        fill = colours[1]))
-  
-  scale_updates <- list(
+  new_scale_functions <- list(
     scale_colour_discrete   = function(...) discrete_scale(  'colour', 'ggthemr', discrete_colours(colours), ...),
     scale_fill_discrete     = function(...) discrete_scale(  'fill',   'ggthemr', discrete_colours(colours), ...),
     scale_colour_continuous = function(...) continuous_scale('colour', 'ggthemr', seq_gradient_pal(palette$gradient[['low']], palette$gradient[['high']]), ...),
@@ -28,10 +12,38 @@ theme_scales <- function(palette, line_weight, pos, envir) {
     scale_fill_gradient     = function(...) continuous_scale('fill',   'ggthemr', seq_gradient_pal(palette$gradient[['low']], palette$gradient[['high']]), ...)
   )
   
-  Map(
-    function (name, f) assign(name, f, envir = envir), 
-    names(scale_updates),
-    scale_updates
+  new_scale_functions
+}
+
+
+theme_geoms <- function(palette, line_weight) {
+  
+  colours <- palette$swatch[-1]
+  
+  new_geom_defaults <- list(
+    # Geoms that only require a default colour.  
+    list(geom = 'abline',    new = list(colour = colours[1])),
+    list(geom = 'point',     new = list(colour = colours[1])),
+    list(geom = 'density',   new = list(colour = colours[1])),
+    list(geom = 'errorbar',  new = list(colour = colours[1])),
+    list(geom = 'errorbarh', new = list(colour = colours[1])),
+    list(geom = 'hline',     new = list(colour = colours[1])),
+    list(geom = 'line',      new = list(colour = colours[1])),
+    
+    list(geom = 'text',      new = list(colour = palette$swatch[1])),
+    
+    # Geoms that only require a default fill.
+    list(geom = 'area',      new = list(fill   = colours[1])),
+    list(geom = 'bar',       new = list(fill   = colours[1])),
+    list(geom = 'dotplot',   new = list(fill   = colours[1])),
+    
+    # Special geoms.
+    list(geom = 'boxplot',   new = list(colour = palette$swatch[1], fill = colours[1], size = line_weight)),
+    list(geom = 'smooth',    new = list(colour = colours[2],        fill = colours[2])),
+    list(geom = 'dotplot',   new = list(colour = colours[1],        fill = colours[1]))
   )
   
+  names(new_geom_defaults) <- vapply(new_geom_defaults, function(x) x$geom, 'temp', USE.NAMES = FALSE)
+  
+  new_geom_defaults
 }
