d35dcce645b148dd77cdf9cefc827f7822db1247
amy17519 <amy17519@gmail.com>
2020-11-10 17:52:56 -0800

    update reset function

--- R/ggthemr_reset.R
+++ R/ggthemr_reset.R
@@ -5,10 +5,12 @@
 ggthemr_reset <- function () {
   
   if (is_ggthemr_active()) {
-  
-    # removing scales
-    scales <- expand.grid(c('colour', 'fill'), c('continuous', 'discrete', 'gradient'))
-    rm(list = apply(scales, 1L, function (x) sprintf('scale_%s_%s', x[1L], x[2L])), envir = .GlobalEnv)
+    
+    # resetting scales
+    options("ggplot2.discrete.fill" = NULL)
+    options("ggplot2.discrete.color" = NULL)
+    options("ggplot2.continuous.fill" = NULL)
+    options("ggplot2.continuous.color" = NULL)
     
     # resetting geoms
     current_theme_info <- get_themr()
