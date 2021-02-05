132fc45b0bbb8a15301eb5f753aff75c2b52139f
Matt <mattias.attbom@gmail.com>
2021-02-05 23:51:44 +0000

    Added roxygen headers to function

--- R/scale_colour_ggthemr_d.R
+++ R/scale_colour_ggthemr_d.R
@@ -1,3 +1,14 @@
+#' ggthemr colour scale
+#'
+#' A utility function for applying the currently selected ggthemr palette to a ggplot object
+#'
+#'
+#'
+#' @export
+#'
+#' @examples
+#' ggplot(mtcars, aes(mpg, disp, colour = factor(am))) + geom_point() + scale_colour_ggthemr_d()
+
 scale_colour_ggthemr_d <- function(...) {
   ggplot2::discrete_scale(
              "colour", "ggthemr",
